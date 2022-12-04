34daa483044d61314a5e8e30a243c60855d9d301
Tamas Kozma <tamas-kozma@outlook.com>
2022-12-04 12:44:14 +0100

    Day 4: Camp Cleanup

---
+++ AdventOfCode2022/Day4CampCleanup/Program.cs
@@ -0,0 +1,29 @@
+﻿using Day4CampCleanup;
+
+string inputFile = (args.Length == 0) ? "input.txt" : args[0];
+var inputLineArray = File.ReadAllLines(inputFile);
+int fullContainmentPairCount = 0;
+int overlapCount = 0;
+foreach (var line in inputLineArray)
+{
+    int commaIndex = line.IndexOf(',');
+    if (commaIndex == -1)
+    {
+        continue;
+    }
+
+    var leftRange = SectionRange.Parse(line.AsSpan(0, commaIndex));
+    var rightRange = SectionRange.Parse(line.AsSpan(commaIndex + 1));
+    if (leftRange.Contains(rightRange) || rightRange.Contains(leftRange))
+    {
+        fullContainmentPairCount++;
+    }
+
+    if (leftRange.Overlaps(rightRange))
+    {
+        overlapCount++;
+    }
+}
+
+Console.WriteLine($"Full containment count: {fullContainmentPairCount}");
+Console.WriteLine($"Overlap count: {overlapCount}");
---
+++ AdventOfCode2022/Day4CampCleanup/SectionRange.cs
@@ -0,0 +1,24 @@
+﻿namespace Day4CampCleanup;
+
+internal readonly record struct SectionRange(
+    int Start,
+    int End)
+{
+    public bool Contains(SectionRange other)
+    {
+        return Start <= other.Start && End >= other.End;
+    }
+
+    public bool Overlaps(SectionRange other)
+    {
+        return End >= other.Start && Start <= other.End;
+    }
+
+    public static SectionRange Parse(ReadOnlySpan<char> input)
+    {
+        int dashIndex = input.IndexOf('-');
+        return new SectionRange(
+            int.Parse(input.Slice(0, dashIndex)),
+            int.Parse(input.Slice(dashIndex + 1)));
+    }
+}
