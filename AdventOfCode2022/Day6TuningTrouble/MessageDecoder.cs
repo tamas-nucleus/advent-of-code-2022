72796b3f99ddeb048df005511ec48db67e03bba7
Tamas Kozma <tamas-kozma@outlook.com>
2022-12-06 22:17:15 +0100

    Day 6: Tuning Trouble

---
+++ AdventOfCode2022/Day6TuningTrouble/MessageDecoder.cs
@@ -0,0 +1,94 @@
+﻿namespace Day6TuningTrouble;
+
+internal sealed class MessageDecoder
+{
+    private const char CharA = 'a';
+    private const int AlphabetLetterCount = 'z' - CharA + 1;
+
+    private readonly int[] letterCounts;
+    private int duplicateCount = 0;
+    private bool needsResetting;
+
+    public MessageDecoder(int windowSize)
+    {
+        WindowSize = windowSize;
+        letterCounts = new int[AlphabetLetterCount];
+    }
+
+    private int WindowSize { get; }
+    public int? MarkerLastCharacterIndex { get; private set; }
+
+    public void ProcessNewMessage(string message)
+    {
+        Reset();
+        needsResetting = true;
+
+        if (message.Length < WindowSize)
+        {
+            return;
+        }
+
+        int letterIndex = 0;
+        for (; letterIndex < WindowSize; letterIndex++)
+        {
+            AddLetter(message[letterIndex]);
+        }
+
+        letterIndex--;
+        while (duplicateCount > 0)
+        {
+            letterIndex++;
+            if (letterIndex == message.Length)
+            {
+                break;
+            }
+
+            RemoveLetter(message[letterIndex - WindowSize]);
+            AddLetter(message[letterIndex]);
+        }
+
+        if (duplicateCount == 0)
+        {
+            MarkerLastCharacterIndex = letterIndex + 1;
+        }
+    }
+
+    private void AddLetter(char letter)
+    {
+        int letterIndex = LetterIndex(letter);
+        if (++letterCounts[letterIndex] == 2)
+        {
+            duplicateCount++;
+        }
+    }
+
+    private void RemoveLetter(char letter)
+    {
+        int letterIndex = LetterIndex(letter);
+        if (--letterCounts[letterIndex] == 1)
+        {
+            duplicateCount--;
+        }
+    }
+
+    private static int LetterIndex(char letter)
+    {
+        return letter - CharA;
+    }
+
+    private void Reset()
+    {
+        if (!needsResetting)
+        {
+            return;
+        }
+
+        for (int i = 0; i < letterCounts.Length; i++)
+        {
+            letterCounts[i] = 0;
+        }
+        duplicateCount = 0;
+        MarkerLastCharacterIndex = null;
+        needsResetting = false;
+    }
+}
---
+++ AdventOfCode2022/Day6TuningTrouble/Program.cs
@@ -0,0 +1,18 @@
+﻿using Day6TuningTrouble;
+
+string inputFile = (args.Length == 0) ? "input.txt" : args[0];
+var inputLineArray = File.ReadAllLines(inputFile);
+var packetStartDecoder = new MessageDecoder(4);
+var messageStartDecoder = new MessageDecoder(14);
+foreach (var message in inputLineArray)
+{
+    if (message.Length == 0)
+    {
+        continue;
+    }
+
+    packetStartDecoder.ProcessNewMessage(message);
+    messageStartDecoder.ProcessNewMessage(message);
+    Console.WriteLine($"first packet marker after character {packetStartDecoder.MarkerLastCharacterIndex}");
+    Console.WriteLine($"first message marker after character {messageStartDecoder.MarkerLastCharacterIndex}");
+}
