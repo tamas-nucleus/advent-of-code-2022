e0776375b972adc38873a382eb7b3ac4de3da922
Tamas Kozma <tamas-kozma@outlook.com>
2022-12-18 01:06:03 +0100

    Day 11: Monkey in the Middle

---
+++ AdventOfCode2022/Day11MonkeyInTheMiddle/Monkey.cs
@@ -0,0 +1,91 @@
+﻿namespace Day11MonkeyInTheMiddle;
+
+internal sealed record Monkey(
+    int MonkeyNumber,
+    char Operation,
+    int? OperationArgument,
+    int TestArgument,
+    int TrueMonkeyNumber,
+    int FalseMonkeyNumber,
+    int InspectionEndReliefDivisor)
+{
+    public Queue<long> ItemQueue { get; } = new Queue<long>();
+    public Monkey? TrueMonkey { get; private set; }
+    public Monkey? FalseMonkey { get; private set; }
+    public int InspectionCount { get; private set; }
+    public long? Modulo { get; private set; }
+
+    public static Monkey Parse(ReadOnlySpan<string> lines, int inspectionEndReliefDivisor)
+    {
+        string operationArgumentString = lines[2]["  Operation: new = old * ".Length..];
+        var monkey = new Monkey(
+            MonkeyNumber: int.Parse(lines[0]["Monkey ".Length..^1]),
+            Operation: lines[2]["  Operation: new = old ".Length],
+            OperationArgument: operationArgumentString == "old" ? null : int.Parse(operationArgumentString),
+            TestArgument: int.Parse(lines[3]["  Test: divisible by ".Length..]),
+            TrueMonkeyNumber: int.Parse(lines[4]["    If true: throw to monkey ".Length..]),
+            FalseMonkeyNumber: int.Parse(lines[5]["    If false: throw to monkey ".Length..]),
+            InspectionEndReliefDivisor: inspectionEndReliefDivisor);
+        var itemList = lines[1]["  Starting items: ".Length..]
+            .Split(", ")
+            .Select(s => long.Parse(s));
+        foreach (var item in itemList)
+        {
+            monkey.ItemQueue.Enqueue(item);
+        }
+        return monkey;
+    }
+
+    public static Monkey Copy(Monkey other, int inspectionEndReliefDivisor)
+    {
+        var copy = new Monkey(
+            other.MonkeyNumber,
+            other.Operation,
+            other.OperationArgument,
+            other.TestArgument,
+            other.TrueMonkeyNumber,
+            other.FalseMonkeyNumber,
+            inspectionEndReliefDivisor);
+        foreach (var item in other.ItemQueue)
+        {
+            copy.ItemQueue.Enqueue(item);
+        }
+        return copy;
+    }
+
+    public void Initialize(IReadOnlyList<Monkey> monkeyList, long? modulo = null)
+    {
+        TrueMonkey = monkeyList[TrueMonkeyNumber];
+        FalseMonkey = monkeyList[FalseMonkeyNumber];
+        Modulo = modulo;
+    }
+
+    public void TakeTurn()
+    {
+        while (ItemQueue.Count > 0)
+        {
+            InspectItem(ItemQueue.Dequeue());
+        }
+    }
+    
+    private void InspectItem(long item)
+    {
+        long actualOperationArgument = OperationArgument ?? item;
+        item = Operation switch
+        {
+            '+' => item + actualOperationArgument,
+            '*' => item * actualOperationArgument,
+            _ => throw new InvalidOperationException()
+        };
+        item /= InspectionEndReliefDivisor;
+        if (Modulo.HasValue)
+        {
+            item %= Modulo.Value;
+        }
+        var targetMonkey = (item % TestArgument) == 0
+            ? TrueMonkey
+            : FalseMonkey;
+        targetMonkey!.ItemQueue.Enqueue(item);
+        InspectionCount++;
+    }
+}
---
+++ AdventOfCode2022/Day11MonkeyInTheMiddle/Program.cs
@@ -0,0 +1,67 @@
+﻿using Day11MonkeyInTheMiddle;
+
+string inputFile = (args.Length == 0) ? "input.txt" : args[0];
+var inputLines = File.ReadAllLines(inputFile);
+var monkeyListWithRelief = new List<Monkey>();
+var monkeyListWithoutRelief = new List<Monkey>();
+long modulo = 1;
+for (int i = 0; i < inputLines.Length; i++)
+{
+    string line = inputLines[i];
+    if (line.Length == 0)
+    {
+        continue;
+    }
+    var monkeyWithRelief = Monkey.Parse(inputLines.AsSpan(i, 6), 3);
+    i += 6;
+    monkeyListWithRelief.Add(monkeyWithRelief);
+
+    var monkeyWithoutRelief = Monkey.Copy(monkeyWithRelief, 1);
+    monkeyListWithoutRelief.Add(monkeyWithoutRelief);
+    modulo *= monkeyWithoutRelief.TestArgument;
+}
+foreach (var monkey in monkeyListWithRelief)
+{
+    monkey.Initialize(monkeyListWithRelief);
+}
+foreach (var monkey in monkeyListWithoutRelief)
+{
+    monkey.Initialize(monkeyListWithoutRelief, modulo);
+}
+for (int i = 0; i < 20; i++)
+{
+    foreach (var monkey in monkeyListWithRelief)
+    {
+        monkey.TakeTurn();
+    }
+}
+for (int i = 0; i < 10000; i++)
+{
+    foreach (var monkey in monkeyListWithoutRelief)
+    {
+        monkey.TakeTurn();
+    }
+}
+
+PrintResults(monkeyListWithRelief);
+Console.WriteLine();
+PrintResults(monkeyListWithoutRelief);
+
+static void PrintResults(IReadOnlyList<Monkey> monkeyList)
+{
+    foreach (var monkey in monkeyList)
+    {
+        PrintInspectionCount(monkey);
+    }
+    long monkeyBusinessLevel = monkeyList
+        .Select(m => (long)m.InspectionCount)
+        .OrderByDescending(c => c)
+        .Take(2)
+        .Aggregate(1l, (a, c) => a * c);
+    Console.WriteLine($"The level of monkey business is {monkeyBusinessLevel}.");
+}
+
+static void PrintInspectionCount(Monkey monkey)
+{
+    Console.WriteLine($"Monkey {monkey.MonkeyNumber} inspected items {monkey.InspectionCount} times.");
+}
