dcd16255dcfb44f3bf31a62da1c878410d7f646f
Tamas Kozma <tamas-kozma@outlook.com>
2022-12-27 01:01:51 +0100

    Day 13: Distress Signal

---
+++ AdventOfCode2022/Day13DistressSignal/Program.cs
@@ -0,0 +1,39 @@
+﻿using Day13DistressSignal;
+
+string inputFile = (args.Length == 0) ? "input.txt" : args[0];
+var inputLineArray = await File.ReadAllLinesAsync(inputFile);
+int rightOrderIndexSum = 0;
+int pairIndex = 1;
+var allPackets = new List<Value>();
+for (int i = 0; i < inputLineArray.Length; i += 3)
+{
+    string firstLine = inputLineArray[i];
+    string secondLine = inputLineArray[i + 1];
+    var left = Value.Parse(firstLine);
+    var right = Value.Parse(secondLine);
+    allPackets.Add(left);
+    allPackets.Add(right);
+    int comparisonResult = left.CompareTo(right);
+    if (comparisonResult <= 0)
+    {
+        rightOrderIndexSum += pairIndex;
+    }
+    pairIndex++;
+}
+
+Console.WriteLine($"Sum of the indices of pairs in right order: {rightOrderIndexSum}");
+
+var dividerArray = new Value[]
+{
+    Value.Parse("[[2]]"),
+    Value.Parse("[[6]]")
+};
+allPackets.AddRange(dividerArray);
+allPackets.Sort();
+int dividerIndicesMultiplied = 1;
+foreach (var divider in dividerArray)
+{
+    dividerIndicesMultiplied *= allPackets.IndexOf(divider) + 1;
+}
+Console.WriteLine($"Divider indices multiplied: {dividerIndicesMultiplied}");
+
---
+++ AdventOfCode2022/Day13DistressSignal/Value.cs
@@ -0,0 +1,137 @@
+﻿namespace Day13DistressSignal;
+
+internal sealed class Value : IComparable<Value>
+{
+    public IReadOnlyList<Value>? ValueList { get; private init; }
+    public int? Number { get; private init; }
+
+    public static Value Parse(string input)
+    {
+        int index = 0;
+        return Parse(input, ref index);
+    }
+
+    public int CompareTo(Value? other)
+    {
+        if (other == null)
+        {
+            throw new ArgumentNullException(nameof(other));
+        }
+
+        if (Number.HasValue && other.Number.HasValue)
+        {
+            return Number.Value.CompareTo(other.Number.Value);
+        }
+        else if (ValueList != null && other.ValueList != null)
+        {
+            return CompareLists(ValueList, other.ValueList);
+        }
+        else if (ValueList != null)
+        {
+            return CompareListWithNumber(ValueList, other);
+        }
+        else
+        {
+            return -1 * CompareListWithNumber(other.ValueList!, this);
+        }
+    }
+
+    public override string ToString()
+    {
+        if (ValueList != null)
+        {
+            return $"[{string.Join(',', ValueList)}]";
+        }
+        else
+        {
+            return Number!.Value.ToString();
+        }
+    }
+
+    private static int CompareLists(
+        IReadOnlyList<Value> left, 
+        IReadOnlyList<Value> right)
+    {
+        int minLength = Math.Min(left.Count, right.Count);
+        for (int i = 0; i < minLength; i++)
+        {
+            int comparisonResult = left[i].CompareTo(right[i]);
+            if (comparisonResult != 0)
+            {
+                return comparisonResult;
+            }
+        }
+
+        return left.Count - right.Count;
+    }
+
+    private static int CompareListWithNumber(
+        IReadOnlyList<Value> left,
+        Value right)
+    {
+        if (left.Count == 0)
+        {
+            return -1;
+        }
+        int comparisonResult = left[0].CompareTo(right);
+        if (comparisonResult != 0)
+        {
+            return comparisonResult;
+        }
+        return left.Count > 1 ? 1 : 0;
+    }
+
+    private static Value Parse(string input, ref int index)
+    {
+        return input[index] switch
+        {
+            '[' => ParseList(input, ref index),
+            _ => ParseNumber(input, ref index)
+        };
+    }
+
+    private static Value ParseList(string input, ref int index)
+    {
+        index++;
+        var valueList = new List<Value>();
+        while (true)
+        {
+            char nextChar = input[index];
+            if (nextChar == ']')
+            {
+                index++;
+                return new Value
+                {
+                    ValueList = valueList
+                };
+            }
+            else if (nextChar == ',')
+            {
+                index++;
+            }
+
+            var value = Parse(input, ref index);
+            valueList.Add(value);
+        }
+    }
+
+    private static Value ParseNumber(string input, ref int index)
+    {
+        int startIndex = index;
+        while (true)
+        {
+            char inputChar = input[index];
+            if (inputChar < '0' || inputChar > '9')
+            {
+                break;
+            }
+            index++;
+        }
+
+        int number = int.Parse(input.AsSpan(startIndex, index - startIndex));
+        return new Value
+        {
+            Number = number
+        };
+    }
+}
